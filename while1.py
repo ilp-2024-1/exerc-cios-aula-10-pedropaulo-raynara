760f797713ef65567dd079973f9342e9180a4b4c
PEDRO PAULO FERREIRA DA SILVA <20241148060021@ifrn.local>
2024-07-11 07:34:00 -0300

    modificação da questão 2

--- while1.py
+++ while1.py
@@ -1,16 +1,19 @@
-
+#questão 1
 num = 1
 while num <= 100:
     print(num)
     num += 1 
 print("Programa encerrado.")
 
+#questão 2 
 num = 1 
 soma = 0
-print ("insira valores")
 while num != 0:
-    num = int(input())
+    num = int(input("insira valores:"))
     soma = soma + num
     if num == 0:
         print("a soma do valores é:", soma)
 print("fim do programa")
+
+#questão 3 
+
