f20462dc2361d1373218c2f66bf84b3bf59ae99d
pedro paulo <pedro.ferreira2@escolar.ifrn.edu.br>
2024-07-10 21:19:43 -0300

    questão 2 feita com comentário adicionados ao código

--- while1.py
+++ while1.py
@@ -4,3 +4,13 @@ while num <= 100:
     print(num)
     num += 1 
 print("Programa encerrado.")
+
+num = 1 
+soma = 0
+print ("insira valores")
+while num != 0:
+    num = int(input())
+    soma = soma + num
+    if num == 0:
+        print("a soma do valores é:", soma)
+print("fim do programa")
